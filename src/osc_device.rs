6d5a9a6ec6b15e1ea83edc1015a56f984632fca7
Benedikt Reinartz <filmor@gmail.com>
2020-06-21 09:09:22 +0200

    Rebuild OscDevice using channels

--- src/osc_device.rs
+++ src/osc_device.rs
@@ -1,20 +1,54 @@
 use crate::codec::OscCodec;
-use futures::stream::StreamExt;
-use futures::SinkExt;
+use futures::{SinkExt, StreamExt};
 use rosc::{OscMessage, OscPacket, OscType};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, pin::Pin};
 use tokio::net::UdpSocket;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+use tokio::task::JoinHandle;
 use tokio_util::udp::UdpFramed;
 
 pub struct OscDevice {
-    framed: UdpFramed<OscCodec>,
-    dest: SocketAddr,
+    //    source: Pin<Arc<dyn Stream<Item = OscPacket>>>,
+    //    sink: Arc<Mutex<Pin<Box<dyn Sink<OscPacket, Error = std::io::Error>>>>>,
+    _source_task: Pin<Box<JoinHandle<()>>>,
+    source_recv: UnboundedReceiver<OscPacket>,
+    _sink_task: Pin<Box<JoinHandle<()>>>,
+    sink_send: UnboundedSender<OscPacket>,
 }
 
 impl OscDevice {
     pub fn new(socket: UdpSocket, dest: SocketAddr) -> Self {
         let framed = UdpFramed::new(socket, OscCodec::new());
-        OscDevice { framed, dest }
+        let (mut sink, mut source) = framed.split();
+
+        // let sink = Mutex::new(Arc::pin(sink.with(|x| async move { Ok((x, dest)) })));
+        // let source = Arc::pin(source.filter_map(|x| async move { x.ok().map(|x| x.0) }));
+
+        let (sink_send, mut sink_recv) = unbounded_channel();
+        let (source_send, source_recv) = unbounded_channel();
+
+        let source_task = tokio::spawn(async move {
+            while let Some(res) = source.next().await {
+                if let Ok((packet, _addr)) = res {
+                    if let Err(_) = source_send.send(packet) {
+                        return;
+                    }
+                }
+            }
+        });
+
+        let sink_task = tokio::spawn(async move {
+            while let Some(res) = sink_recv.next().await {
+                let _res = sink.send((res, dest)).await;
+            }
+        });
+
+        OscDevice {
+            _source_task: Box::pin(source_task),
+            source_recv,
+            _sink_task: Box::pin(sink_task),
+            sink_send,
+        }
     }
 
     pub async fn send_msg(&mut self, addr: &str, args: Vec<OscType>) {
@@ -22,10 +56,10 @@ impl OscDevice {
         let msg = OscMessage { addr, args };
         let msg = OscPacket::Message(msg);
 
-        let _res = self.framed.send((msg, self.dest)).await;
+        let _res = self.sink_send.send(msg);
     }
 
     pub async fn receive_msg(&mut self) -> Option<OscPacket> {
-        self.framed.next().await?.ok().map(|(packet, _addr)| packet)
+        self.source_recv.next().await
     }
 }
